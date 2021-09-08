848889590856ff132a4aa1800289feb9e61081b7
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-08 10:42:25 -0300

    Jest config

---
+++ myDrive-api/jest.config.mjs
@@ -0,0 +1,28 @@
+export default {
+  clearMocks: true,
+  restoreMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+
+  coverageReporters: [
+    "text",
+    "lcov"
+  ],
+  textEnviroment: "node",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  watchPathIgnorePatterns: [
+    "node_modules"
+  ],
+  trasformIgnorePatterns: ["node_modules"],
+  collectCoverageFrom: [
+    "src/**/*.js", "!src/**/index.js"
+  ]
+};
